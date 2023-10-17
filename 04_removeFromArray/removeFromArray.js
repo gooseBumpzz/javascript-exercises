e97f9bfd8a1c9d31b6c673117887bb54fada9de7
gooseBumpzz <142624928+gooseBumpzz@users.noreply.github.com>
2023-10-17 16:52:25 -0400

    finished 4 assignments and passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(enterArray, itemRemove) {
+    for (let i = 1; i < arguments.length ; i++){
+        if (!enterArray.includes(arguments[i])){ //continue to run if no arguments found in the Array
+            continue;
+        }
+        let itemIndex = enterArray.indexOf(arguments[i]);
+        enterArray.splice(itemIndex, 1);  
+    }
+    return enterArray;
+}    
+removeFromArray([1, 2, 3, 4], 3)
 
-};
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
