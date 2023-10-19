e0d6ac524f156b2be80cb791c9e78e23c8daf7a7
gooseBumpzz <142624928+gooseBumpzz@users.noreply.github.com>
2023-10-19 09:48:02 -0400

    finished assignments 5-7. Passed all tests.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 
+    if (year%400 == 0) return true;
+    else if (year%400 == 0 && year%4 ==0) return true;
+    else if (year%4 ==0 && year%100 ==0  &&year%400 != 0) return false;
+    else if (year%4 ==0 && year%100 !=0) return true;
+    else return false;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF-32)*5/9;
+  roundedC = Math.round(tempC*10)/10;
+  return roundedC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = tempC*9/5 +32;
+  roundedF = Math.round(tempF*10)/10;
+  return roundedF;
 };
 
 // Do not edit below this line
