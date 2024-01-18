25e591da6815189884811c44b01a009a49e59cfb
gooseBumpzz <142624928+gooseBumpzz@users.noreply.github.com>
2024-01-18 14:48:56 -0500

    passsed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,56 @@
-const add = function() {
+const add = function(a, b) {
+	return a+b;
+};
+
+const subtract = function(a, b) {
+	return a-b;
+};
+
+const sum = function(arr) {
+	let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+
+  // const sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  // console.log(sum);
+};
+
+
+
+const multiply = function(arr) {
+  // let res = 1;
+  // for (let i = 0; i < arr.length; i++) {
+  //   res = res* arr[i];
+  // }
+  // return res;
+
+  const res =arr.reduce((accumulator, curVal) => accumulator * curVal);
+  return res;
+  console.log(res);
+
+};
+
+const power = function(a, b) {
+//   let res = 1;
+//   for(let i =1; i <=b; i++){
+//     res = res*a;
+//   }
+//   return res;
 	
+  return Math.pow(a, b);
 };
 
-const subtract = function() {
-	
+const factorial = function(a) {
+  let res = 1;
+  for(let i =0; a >0; i++){
+    res = res*a;
+    a--;
+  }
+  return res;
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
