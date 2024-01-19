785eabde8c709fecfef898dd3909d87b188301b8
gooseBumpzz <142624928+gooseBumpzz@users.noreply.github.com>
2024-01-19 08:03:48 -0500

    finished exersices 8-11

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
+const palindromes = function (stringName) {
+    const original = stringName.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    const reversed = original.split("").reverse("").join("");
+    return original == reversed;
+ };
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    //check for negative number or 0
+    if (num <0) return "OOPS";
+    if (num === 0) return 0;
+    // create and popuilate a Fibonacci array 
+    const fibArray = [1,1];
+    for (let i = 2; i <= num; i++) {
+        fibArray[i] = fibArray[i-2]+fibArray[i-1];  
+    }
+    // return the Nth number from the array
+    return fibArray[fibArray.length -2];
 };
+//console.log(fibonacci(10));
 
 // Do not edit below this line
 module.exports = fibonacci;
