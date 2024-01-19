0cbfcee3d717a3667c6c3b0d99e70ba125887aa7
gooseBumpzz <142624928+gooseBumpzz@users.noreply.github.com>
2024-01-19 07:58:15 -0500

    finished exercises 8-11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titleArray =[]
+    for (let i = 0; i < books.length; i++) {
+    titleArray[i] = books[i].title;
+   }
+    return titleArray;
+    // shorter version
+    //return books.map((book) => book.title);
 };
 
 // Do not edit below this line
