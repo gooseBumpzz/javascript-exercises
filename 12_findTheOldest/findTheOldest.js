9a88eeabff6f5fb718fd3b15e714761bb768a170
gooseBumpzz <142624928+gooseBumpzz@users.noreply.github.com>
2024-01-22 14:59:01 -0500

    pass every test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = 0;
+    let oldObj = {};
+    let currentOldest = 0;
+      for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath === undefined){
+          currentOldest = new Date().getFullYear() - people[i].yearOfBirth;
+        }
+        else {
+          currentOldest = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+        if (currentOldest >oldest){
+          oldest = currentOldest;
+          oldObj = people[i];
+        }
+     }
+    return oldObj;
 };
 
 // Do not edit below this line
